f68ed5bcca92b80e3ccf8dfc02f9da34b48c5520
Nico00190 <1129@htl.rennweg.at>
2023-05-11 12:05:17 +0200

    Neu

---
+++ military-tower-defense/Assets/Scripts/PanzerObject.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PfeilScript : MonoBehaviour
+{
+    
+        public string targetTag = "Bloon";
+    public float rotationSpeed = 5f;
+    public float range = 10f;
+    public GameObject projectilePrefab;
+    public float fireRate = 1f;
+    public Vector3 projectileOffset;
+
+    private Transform target;
+    private float fireTimer = 0f;
+
+    void Update()
+    {
+        // Suche nach dem Ziel anhand des Tags
+        if (target == null)
+        {
+            GameObject targetObject = GameObject.FindGameObjectWithTag(targetTag);
+            if (targetObject != null)
+            {
+                target = targetObject.transform;
+            }
+        }
+
+        // Orientierung des Panzers zum Ziel
+        if (target != null)
+        {
+            Vector3 direction = target.position - transform.position;
+            if (direction.magnitude <= range)
+            {
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+                Quaternion lookRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+            }
+        }
+
+        // Spawnen von Raketen/Pfeilen
+        fireTimer += Time.deltaTime;
+        if (fireTimer >= fireRate)
+        {
+            fireTimer = 0f;
+            Instantiate(projectilePrefab, transform.position + projectileOffset, transform.rotation);
+        }
+    }
+}
+
+
